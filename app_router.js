35b481007212c66398716b0daf2ccc4830ef2acd
sohanchang <sohanchang@gmail.com>
2019-06-26 15:21:30 +0800

    reply message version1.0.15

--- app_router.js
+++ app_router.js
@@ -14,25 +14,23 @@ const linebotParser = bot.parser();
 app.post('/linewebhook', linebotParser);
 
 bot.on('message', function (event) {
- //   if (source.userId) {
- //       return client.getProfile(source.userId)
- //         .then((profile) => replyText(
- //           replyToken,
- //           [
- //             `Display name: ${profile.displayName}`,
- //             `Status message: ${profile.statusMessage}`,
- //           ]
- //         ));
- //     } else {
- //       return replyText(replyToken, 'Bot can\'t use profile API without user ID');
- //     };
- //       event.message.text
-        event.reply(event.message.text+" 003!").then(function (data) {
+    if (source.userId) {
+        return client.getProfile(source.userId)
+          .then((profile) => replyText(
+            replyToken,
+            [
+              `Display name: ${profile.displayName}`,
+              `Status message: ${profile.statusMessage}`,
+            ]
+          ));
+      } else {
+        return replyText(replyToken, 'Bot can\'t use profile API without user ID');
+      };
+
+        event.reply(event.message.text+"  OK!  "+profile.displayName).then(function (data) {
                             console.log('Success', data);
-                            event.reply(event.message.text+" OK!");
                         }).catch(function (error) {
                              console.log('Error', error);
-                             event.reply(event.message.text+" Error!");
                              });           
 });
 
