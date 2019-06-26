7de7e8502238164d3ec087f6f14a483342dfc278
sohanchang <sohanchang@gmail.com>
2019-06-26 12:14:56 +0800

    reply message version1.0.1

--- app_router.js
+++ app_router.js
@@ -14,43 +14,52 @@ const linebotParser = bot.parser();
 app.post('/linewebhook', linebotParser);
 
 bot.on('message', function (event) {
-    var gmessagetext = event.message.text;
-    switch (gmessagetext){
-        case "T1":
-            event.reply(event.message.text+" Ya!  01").then(function (data) {
-                console.log('Success', data);
-            }).catch(function (error) {
-                console.log('Error', error);
-            });  
-        break;
-        case "T3":
-            event.reply(event.message.text+" Ya!  03").then(function (data) {
-                console.log('Success', data);
-            }).catch(function (error) {
-                console.log('Error', error);
-            });  
-        break;
-        case "T5":
-            event.reply(event.message.text+" Ya!  05").then(function (data) {
-                console.log('Success', data);
-            }).catch(function (error) {
-                console.log('Error', error);
-            });  
-        break;
-        case "T7":
-            event.reply(event.message.text+" Ya!  07").then(function (data) {
-                console.log('Success', data);
-            }).catch(function (error) {
-                console.log('Error', error);
-            });  
-        break;
+    switch (event.type) {
+        case 'message':
+          const message = event.message;
+          switch (message.type) {
+            case 'text':
+              if message.text = "test",  
+              return handleText(message, event.replyToken, event.source);
+            case 'image':
+              return handleImage(message, event.replyToken);
+    //        case 'video':
+    //          return handleVideo(message, event.replyToken);
+    //        case 'audio':
+    //          return handleAudio(message, event.replyToken);
+    //        case 'location':
+    //          return handleLocation(message, event.replyToken);
+    //        case 'sticker':
+    //          return handleSticker(message, event.replyToken);
+            default:
+              throw new Error(`Unknown message: ${JSON.stringify(message)}`);
+          }
+    
+    //    case 'follow':
+    //      return replyText(event.replyToken, 'Got followed event');
+    
+    //    case 'unfollow':
+    //      return console.log(`Unfollowed this bot: ${JSON.stringify(event)}`);
+    
+    //    case 'join':
+    //      return replyText(event.replyToken, `Joined ${event.source.type}`);
+    
+    //    case 'leave':
+    //      return console.log(`Left: ${JSON.stringify(event)}`);
+    
+    //    case 'postback':
+    //      let data = event.postback.data;
+    //      if (data === 'DATE' || data === 'TIME' || data === 'DATETIME') {
+    //        data += `(${JSON.stringify(event.postback.params)})`;
+    //      }
+    //      return replyText(event.replyToken, `Got postback: ${data}`);
+    
+    //    case 'beacon':
+    //      return replyText(event.replyToken, `Got beacon: ${event.beacon.hwid}`);
+    
         default:
-            event.reply(event.message.text).then(function (data) {
-                console.log('Success', data);
-            }).catch(function (error) {
-                console.log('Error', error);
-            });
-    }
+          throw new Error(`Unknown event: ${JSON.stringify(event)}`);
+      }
 });    
 
 bot.on('message', function (event) {
@@ -70,3 +79,186 @@ app.get('/', function (request, response) {
 app.post('/ajax', function (request, response) {
     response.send("response by ajax");
 });
+
+function handleText(message, replyToken, source) {
+    const buttonsImageURL = `${baseURL}/static/buttons/1040.jpg`;
+  
+    switch (message.text) {
+      case 'profile':
+        if (source.userId) {
+          return client.getProfile(source.userId)
+            .then((profile) => replyText(
+              replyToken,
+              [
+                `Display name: ${profile.displayName}`,
+                `Status message: ${profile.statusMessage}`,
+              ]
+            ));
+        } else {
+          return replyText(replyToken, 'Bot can\'t use profile API without user ID');
+        }
+      case 'buttons':
+        return client.replyMessage(
+          replyToken,
+          {
+            type: 'template',
+            altText: 'Buttons alt text',
+            template: {
+              type: 'buttons',
+              thumbnailImageUrl: buttonsImageURL,
+              title: 'My button sample',
+              text: 'Hello, my button',
+              actions: [
+                { label: 'Go to line.me', type: 'uri', uri: 'https://line.me' },
+                { label: 'Say hello1', type: 'postback', data: 'hello こんにちは' },
+                { label: '言 hello2', type: 'postback', data: 'hello こんにちは', text: 'hello こんにちは' },
+                { label: 'Say message', type: 'message', text: 'Rice=米' },
+              ],
+            },
+          }
+        );
+      case 'confirm':
+        return client.replyMessage(
+          replyToken,
+          {
+            type: 'template',
+            altText: 'Confirm alt text',
+            template: {
+              type: 'confirm',
+              text: 'Do it?',
+              actions: [
+                { label: 'Yes', type: 'message', text: 'Yes!' },
+                { label: 'No', type: 'message', text: 'No!' },
+              ],
+            },
+          }
+        )
+      case 'carousel':
+        return client.replyMessage(
+          replyToken,
+          {
+            type: 'template',
+            altText: 'Carousel alt text',
+            template: {
+              type: 'carousel',
+              columns: [
+                {
+                  thumbnailImageUrl: buttonsImageURL,
+                  title: 'hoge',
+                  text: 'fuga',
+                  actions: [
+                    { label: 'Go to line.me', type: 'uri', uri: 'https://line.me' },
+                    { label: 'Say hello1', type: 'postback', data: 'hello こんにちは' },
+                  ],
+                },
+                {
+                  thumbnailImageUrl: buttonsImageURL,
+                  title: 'hoge',
+                  text: 'fuga',
+                  actions: [
+                    { label: '言 hello2', type: 'postback', data: 'hello こんにちは', text: 'hello こんにちは' },
+                    { label: 'Say message', type: 'message', text: 'Rice=米' },
+                  ],
+                },
+              ],
+            },
+          }
+        );
+      case 'image carousel':
+        return client.replyMessage(
+          replyToken,
+          {
+            type: 'template',
+            altText: 'Image carousel alt text',
+            template: {
+              type: 'image_carousel',
+              columns: [
+                {
+                  imageUrl: buttonsImageURL,
+                  action: { label: 'Go to LINE', type: 'uri', uri: 'https://line.me' },
+                },
+                {
+                  imageUrl: buttonsImageURL,
+                  action: { label: 'Say hello1', type: 'postback', data: 'hello こんにちは' },
+                },
+                {
+                  imageUrl: buttonsImageURL,
+                  action: { label: 'Say message', type: 'message', text: 'Rice=米' },
+                },
+                {
+                  imageUrl: buttonsImageURL,
+                  action: {
+                    label: 'datetime',
+                    type: 'datetimepicker',
+                    data: 'DATETIME',
+                    mode: 'datetime',
+                  },
+                },
+              ]
+            },
+          }
+        );
+      case 'datetime':
+        return client.replyMessage(
+          replyToken,
+          {
+            type: 'template',
+            altText: 'Datetime pickers alt text',
+            template: {
+              type: 'buttons',
+              text: 'Select date / time !',
+              actions: [
+                { type: 'datetimepicker', label: 'date', data: 'DATE', mode: 'date' },
+                { type: 'datetimepicker', label: 'time', data: 'TIME', mode: 'time' },
+                { type: 'datetimepicker', label: 'datetime', data: 'DATETIME', mode: 'datetime' },
+              ],
+            },
+          }
+        );
+      case 'imagemap':
+        return client.replyMessage(
+          replyToken,
+          {
+            type: 'imagemap',
+            baseUrl: `${baseURL}/static/rich`,
+            altText: 'Imagemap alt text',
+            baseSize: { width: 1040, height: 1040 },
+            actions: [
+              { area: { x: 0, y: 0, width: 520, height: 520 }, type: 'uri', linkUri: 'https://store.line.me/family/manga/en' },
+              { area: { x: 520, y: 0, width: 520, height: 520 }, type: 'uri', linkUri: 'https://store.line.me/family/music/en' },
+              { area: { x: 0, y: 520, width: 520, height: 520 }, type: 'uri', linkUri: 'https://store.line.me/family/play/en' },
+              { area: { x: 520, y: 520, width: 520, height: 520 }, type: 'message', text: 'URANAI!' },
+            ],
+            video: {
+              originalContentUrl: `${baseURL}/static/imagemap/video.mp4`,
+              previewImageUrl: `${baseURL}/static/imagemap/preview.jpg`,
+              area: {
+                x: 280,
+                y: 385,
+                width: 480,
+                height: 270,
+              },
+              externalLink: {
+                linkUri: 'https://line.me',
+                label: 'LINE'
+              }
+            },
+          }
+        );
+      case 'bye':
+        switch (source.type) {
+          case 'user':
+            return replyText(replyToken, 'Bot can\'t leave from 1:1 chat');
+          case 'group':
+            return replyText(replyToken, 'Leaving group')
+              .then(() => client.leaveGroup(source.groupId));
+          case 'room':
+            return replyText(replyToken, 'Leaving room')
+              .then(() => client.leaveRoom(source.roomId));
+        }
+      default:
+        console.log(`Echo message to ${replyToken}: ${message.text}`);
+        return replyText(replyToken, message.text);
+    }
+  }
+  
