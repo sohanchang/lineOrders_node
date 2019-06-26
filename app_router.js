28cab70354b22f5a62258cbb8c5d797faf536a3b
sohanchang <sohanchang@gmail.com>
2019-06-26 15:38:54 +0800

    reply message version1.0.17

--- app_router.js
+++ app_router.js
@@ -5,8 +5,9 @@ const express = require('express');
 
 // create LINE SDK config from env variables
 const config = {
-  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
+  channelId: process.env.CHANNEL_ID,
   channelSecret: process.env.CHANNEL_SECRET,
+  channelAccessToken: process.env.CHANNEL_TOKEN,  
 };
 
 // create LINE SDK client
