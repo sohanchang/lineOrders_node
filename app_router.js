23fc03e548f0b4fadd60e123556afc284629668d
sohanchang <sohanchang@gmail.com>
2019-06-27 15:04:26 +0800

    reply message version1.0.31

--- app_router.js
+++ app_router.js
@@ -5,7 +5,7 @@ const express = require('express');
 const fs = require('fs');
 const path = require('path');
 const cp = require('child_process');
-const ngrok = require('ngrok');
+// const ngrok = require('ngrok');
 
 // create LINE SDK config from env variables
 const config = {
