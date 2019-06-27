40329227e32f2cf8f3f943851282d2069fa749ca
sohanchang <sohanchang@gmail.com>
2019-06-27 15:36:28 +0800

    reply message version1.0.36

--- app_router.js
+++ app_router.js
@@ -24,6 +24,13 @@ const client = new line.Client(config);
 // about Express itself: https://expressjs.com/
 const app = express();
 
+//因為 express 預設走 port 3000，而 heroku 上預設卻不是，要透過下列程式轉換
+var server = app.listen(process.env.PORT || 8080, function() {
+    var port = server.address().port;
+    console.log("App now running on port", port);
+  });
+
+
 // serve static and downloaded files
 app.use('/static', express.static('static'));
 app.use('/downloaded', express.static('downloaded'));
