c6249b557ffe342f975cdbddcb6b3b03c6d224d1
sohanchang <sohanchang@gmail.com>
2019-06-27 17:21:18 +0800

    reply message version1.0.41

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ var app = require('./app_router.js')
 //As a global, it is always available to Node.js applications without using require().
 //The 'process.env' property returns an object containing the user environment.
 //you can set the environment variable 'PORT' to tell your web server what port to listen on.
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3000;
 //whatever is in the environment variable 'PORT', or 3000 if there's nothing there
 
 //binds and listens for connections on the specified host and port. This method is identical to Node’s http.Server.listen().
--- app_router.js
+++ app_router.js
@@ -1,449 +1,80 @@
-'use strict';
-
 const line = require('@line/bot-sdk');
 const express = require('express');
-const fs = require('fs');
-const path = require('path');
-const cp = require('child_process');
-// const ngrok = require('ngrok');
+const linebot = require('linebot');
+var app = module.exports = express();
 
-// create LINE SDK config from env variables
-const config = {
+const bot = linebot({
     channelId: process.env.CHANNEL_ID,
     channelSecret: process.env.CHANNEL_SECRET,
     channelAccessToken: process.env.CHANNEL_TOKEN
-};
-
-// base URL for webhook server
-let baseURL = process.env.BASE_URL;
-
-// create LINE SDK client
-const client = new line.Client(config);
-
-// create Express app
-// about Express itself: https://expressjs.com/
-const app = express();
-
-//因為 express 預設走 port 3000，而 heroku 上預設卻不是，要透過下列程式轉換
-var server = app.listen(process.env.PORT || 8080, function() {
-    var port = server.address().port;
-    console.log("App now running on port", port);
-  });
-
-
-// serve static and downloaded files
-app.use('/static', express.static('static'));
-app.use('/downloaded', express.static('downloaded'));
-
-app.get('/callback', (req, res) => res.end(`I'm listening. Please access with POST.`));
-
-// webhook callback
-app.post('/callback', line.middleware(config), (req, res) => {
-  if (req.body.destination) {
-    console.log("Destination User ID: " + req.body.destination);
-  }
-
-  // req.body.events should be an array of events
-  if (!Array.isArray(req.body.events)) {
-    return res.status(500).end();
-  }
-
-  // handle events separately
-  Promise.all(req.body.events.map(handleEvent))
-    .then(() => res.end())
-    .catch((err) => {
-      console.error(err);
-      res.status(500).end();
-    });
 });
 
-// simple reply function
-const replyText = (token, texts) => {
-  texts = Array.isArray(texts) ? texts : [texts];
-  return client.replyMessage(
-    token,
-    texts.map((text) => ({ type: 'text', text }))
-  );
-};
+const linebotParser = bot.parser();
 
-// callback function to handle a single event
-function handleEvent(event) {
-  if (event.replyToken && event.replyToken.match(/^(.)\1*$/)) {
-    return console.log("Test hook recieved: " + JSON.stringify(event.message));
-  }
+// Redirect linebot webhook url to linebot parser
+app.post('/linewebhook', linebotParser);
 
-  switch (event.type) {
-    case 'message':
-      const message = event.message;
-      switch (message.type) {
-        case 'text':
-          return handleText(message, event.replyToken, event.source);
-        case 'image':
-          return handleImage(message, event.replyToken);
-        case 'video':
-          return handleVideo(message, event.replyToken);
-        case 'audio':
-          return handleAudio(message, event.replyToken);
-        case 'location':
-          return handleLocation(message, event.replyToken);
-        case 'sticker':
-          return handleSticker(message, event.replyToken);
-        default:
-          throw new Error(`Unknown message: ${JSON.stringify(message)}`);
-      }
-
-    case 'follow':
-      return replyText(event.replyToken, 'Got followed event');
-
-    case 'unfollow':
-      return console.log(`Unfollowed this bot: ${JSON.stringify(event)}`);
-
-    case 'join':
-      return replyText(event.replyToken, `Joined ${event.source.type}`);
-
-    case 'leave':
-      return console.log(`Left: ${JSON.stringify(event)}`);
-
-    case 'postback':
-      let data = event.postback.data;
-      if (data === 'DATE' || data === 'TIME' || data === 'DATETIME') {
-        data += `(${JSON.stringify(event.postback.params)})`;
-      }
-      return replyText(event.replyToken, `Got postback: ${data}`);
-
-    case 'beacon':
-      return replyText(event.replyToken, `Got beacon: ${event.beacon.hwid}`);
-
-    default:
-      throw new Error(`Unknown event: ${JSON.stringify(event)}`);
-  }
-}
-
-function handleText(message, replyToken, source) {
-  const buttonsImageURL = `${baseURL}/static/buttons/1040.jpg`;
-
-  switch (message.text) {
-    case 'profile':
-      if (source.userId) {
-        return client.getProfile(source.userId)
-          .then((profile) => replyText(
-            replyToken,
-            [
-              `Display name: ${profile.displayName}`,
-              `Status message: ${profile.statusMessage}`,
-            ]
-          ));
-      } else {
-        return replyText(replyToken, 'Bot can\'t use profile API without user ID');
-      }
-    case 'buttons':
-      return client.replyMessage(
-        replyToken,
-        {
-          type: 'template',
-          altText: 'Buttons alt text',
-          template: {
-            type: 'buttons',
-            thumbnailImageUrl: buttonsImageURL,
-            title: 'My button sample',
-            text: 'Hello, my button',
-            actions: [
-              { label: 'Go to line.me', type: 'uri', uri: 'https://line.me' },
-              { label: 'Say hello1', type: 'postback', data: 'hello こんにちは' },
-              { label: '言 hello2', type: 'postback', data: 'hello こんにちは', text: 'hello こんにちは' },
-              { label: 'Say message', type: 'message', text: 'Rice=米' },
-            ],
-          },
-        }
-      );
-    case 'confirm':
-      return client.replyMessage(
-        replyToken,
-        {
-          type: 'template',
-          altText: 'Confirm alt text',
-          template: {
-            type: 'confirm',
-            text: 'Do it?',
-            actions: [
-              { label: 'Yes', type: 'message', text: 'Yes!' },
-              { label: 'No', type: 'message', text: 'No!' },
-            ],
-          },
-        }
-      )
-    case 'carousel':
-      return client.replyMessage(
-        replyToken,
-        {
-          type: 'template',
-          altText: 'Carousel alt text',
-          template: {
-            type: 'carousel',
-            columns: [
-              {
-                thumbnailImageUrl: buttonsImageURL,
-                title: 'hoge',
-                text: 'fuga',
-                actions: [
-                  { label: 'Go to line.me', type: 'uri', uri: 'https://line.me' },
-                  { label: 'Say hello1', type: 'postback', data: 'hello こんにちは' },
-                ],
-              },
-              {
-                thumbnailImageUrl: buttonsImageURL,
-                title: 'hoge',
-                text: 'fuga',
-                actions: [
-                  { label: '言 hello2', type: 'postback', data: 'hello こんにちは', text: 'hello こんにちは' },
-                  { label: 'Say message', type: 'message', text: 'Rice=米' },
-                ],
-              },
-            ],
-          },
-        }
-      );
-    case 'image carousel':
-      return client.replyMessage(
-        replyToken,
-        {
-          type: 'template',
-          altText: 'Image carousel alt text',
-          template: {
-            type: 'image_carousel',
-            columns: [
-              {
-                imageUrl: buttonsImageURL,
-                action: { label: 'Go to LINE', type: 'uri', uri: 'https://line.me' },
-              },
-              {
-                imageUrl: buttonsImageURL,
-                action: { label: 'Say hello1', type: 'postback', data: 'hello こんにちは' },
-              },
-              {
-                imageUrl: buttonsImageURL,
-                action: { label: 'Say message', type: 'message', text: 'Rice=米' },
-              },
-              {
-                imageUrl: buttonsImageURL,
-                action: {
-                  label: 'datetime',
-                  type: 'datetimepicker',
-                  data: 'DATETIME',
-                  mode: 'datetime',
-                },
-              },
-            ]
-          },
-        }
-      );
-    case 'datetime':
-      return client.replyMessage(
-        replyToken,
-        {
-          type: 'template',
-          altText: 'Datetime pickers alt text',
-          template: {
-            type: 'buttons',
-            text: 'Select date / time !',
-            actions: [
-              { type: 'datetimepicker', label: 'date', data: 'DATE', mode: 'date' },
-              { type: 'datetimepicker', label: 'time', data: 'TIME', mode: 'time' },
-              { type: 'datetimepicker', label: 'datetime', data: 'DATETIME', mode: 'datetime' },
-            ],
-          },
-        }
-      );
-    case 'imagemap':
-      return client.replyMessage(
-        replyToken,
-        {
-          type: 'imagemap',
-          baseUrl: `${baseURL}/static/rich`,
-          altText: 'Imagemap alt text',
-          baseSize: { width: 1040, height: 1040 },
-          actions: [
-            { area: { x: 0, y: 0, width: 520, height: 520 }, type: 'uri', linkUri: 'https://store.line.me/family/manga/en' },
-            { area: { x: 520, y: 0, width: 520, height: 520 }, type: 'uri', linkUri: 'https://store.line.me/family/music/en' },
-            { area: { x: 0, y: 520, width: 520, height: 520 }, type: 'uri', linkUri: 'https://store.line.me/family/play/en' },
-            { area: { x: 520, y: 520, width: 520, height: 520 }, type: 'message', text: 'URANAI!' },
-          ],
-          video: {
-            originalContentUrl: `${baseURL}/static/imagemap/video.mp4`,
-            previewImageUrl: `${baseURL}/static/imagemap/preview.jpg`,
-            area: {
-              x: 280,
-              y: 385,
-              width: 480,
-              height: 270,
+bot.on('message', function (event) {
+    // if (source.userId) {
+    //     return client.getProfile(source.userId)
+    //       .then((profile) => replyText(
+    //         replyToken,
+    //         [
+    //           `Display name: ${profile.displayName}`,
+    //           `Status message: ${profile.statusMessage}`,
+    //         ]
+    //       ));
+    //   } else {
+    //     return replyText(replyToken, 'Bot can\'t use profile API without user ID');
+    //   };
+    // usertemp = 
+    buttonsImageURL = 'https://gam.jzm.mybluehost.me/pictures/line/0d50ed3b-f5ff-4a5a-bd81-bd35970e814c.png';
+    msg1 ={
+        type: 'template',
+        altText: 'Carousel alt text',
+        template: {
+          type: 'carousel',
+          columns: [
+            {
+              thumbnailImageUrl: buttonsImageURL,
+              title: 'hoge',
+              text: 'fuga',
+              actions: [
+                { label: 'Go to web', type: 'uri', uri: 'http://220.135.224.39/index.htm' },
+                { label: 'Say hello1', type: 'postback', data: 'hello こんにちは', text: 'hello 1' },
+              ],
             },
-            externalLink: {
-              linkUri: 'https://line.me',
-              label: 'LINE'
-            }
-          },
-        }
-      );
-    case 'bye':
-      switch (source.type) {
-        case 'user':
-          return replyText(replyToken, 'Bot can\'t leave from 1:1 chat');
-        case 'group':
-          return replyText(replyToken, 'Leaving group')
-            .then(() => client.leaveGroup(source.groupId));
-        case 'room':
-          return replyText(replyToken, 'Leaving room')
-            .then(() => client.leaveRoom(source.roomId));
-      }
-    default:
-      console.log(`Echo message to ${replyToken}: ${message.text}`);
-      return replyText(replyToken, message.text);
-  }
-}
+            {
+              thumbnailImageUrl: buttonsImageURL,
+              title: 'hoge2',
+              text: 'fuga2',
+              actions: [
+                { label: '言 hello2', type: 'postback', data: 'hello こんにちは', text: 'hello こんにちは' },
+                { label: 'Say message', type: 'message', text: 'Rice=米' },
+              ],
+            },
+          ],
+        },
+    };   
+        msg = event.message.text;
+        if (msg == '2go') {
+        event.reply(msg1).then(function (data) {
+                            console.log('Success', data);
+                        }).catch(function (error) {
+                             console.log('Error', error);
+                             });         
+                            }
 
-function handleImage(message, replyToken) {
-  let getContent;
-  if (message.contentProvider.type === "line") {
-    const downloadPath = path.join(__dirname, 'downloaded', `${message.id}.jpg`);
-    const previewPath = path.join(__dirname, 'downloaded', `${message.id}-preview.jpg`);
+});
 
-    getContent = downloadContent(message.id, downloadPath)
-      .then((downloadPath) => {
-        // ImageMagick is needed here to run 'convert'
-        // Please consider about security and performance by yourself
-        cp.execSync(`convert -resize 240x jpeg:${downloadPath} jpeg:${previewPath}`);
 
-        return {
-          originalContentUrl: baseURL + '/downloaded/' + path.basename(downloadPath),
-          previewImageUrl: baseURL + '/downloaded/' + path.basename(previewPath),
-        };
-      });
-  } else if (message.contentProvider.type === "external") {
-    getContent = Promise.resolve(message.contentProvider);
-  }
 
-  return getContent
-    .then(({ originalContentUrl, previewImageUrl }) => {
-      return client.replyMessage(
-        replyToken,
-        {
-          type: 'image',
-          originalContentUrl,
-          previewImageUrl,
-        }
-      );
-    });
-}
+//Express API --------- App.get('path', callback function);
+//routes HTTP GET requests to the specified path with the specified callback functions
+app.get('/', function (request, response) {
+    response.json({ message: 'response from node service!' });
+});
 
-function handleVideo(message, replyToken) {
-  let getContent;
-  if (message.contentProvider.type === "line") {
-    const downloadPath = path.join(__dirname, 'downloaded', `${message.id}.mp4`);
-    const previewPath = path.join(__dirname, 'downloaded', `${message.id}-preview.jpg`);
-
-    getContent = downloadContent(message.id, downloadPath)
-      .then((downloadPath) => {
-        // FFmpeg and ImageMagick is needed here to run 'convert'
-        // Please consider about security and performance by yourself
-        cp.execSync(`convert mp4:${downloadPath}[0] jpeg:${previewPath}`);
-
-        return {
-          originalContentUrl: baseURL + '/downloaded/' + path.basename(downloadPath),
-          previewImageUrl: baseURL + '/downloaded/' + path.basename(previewPath),
-        }
-      });
-  } else if (message.contentProvider.type === "external") {
-    getContent = Promise.resolve(message.contentProvider);
-  }
-
-  return getContent
-    .then(({ originalContentUrl, previewImageUrl }) => {
-      return client.replyMessage(
-        replyToken,
-        {
-          type: 'video',
-          originalContentUrl,
-          previewImageUrl,
-        }
-      );
-    });
-}
-
-function handleAudio(message, replyToken) {
-  let getContent;
-  if (message.contentProvider.type === "line") {
-    const downloadPath = path.join(__dirname, 'downloaded', `${message.id}.m4a`);
-
-    getContent = downloadContent(message.id, downloadPath)
-      .then((downloadPath) => {
-        return {
-            originalContentUrl: baseURL + '/downloaded/' + path.basename(downloadPath),
-        };
-      });
-  } else {
-    getContent = Promise.resolve(message.contentProvider);
-  }
-
-  return getContent
-    .then(({ originalContentUrl }) => {
-      return client.replyMessage(
-        replyToken,
-        {
-          type: 'audio',
-          originalContentUrl,
-          duration: message.duration,
-        }
-      );
-    });
-}
-
-function downloadContent(messageId, downloadPath) {
-  return client.getMessageContent(messageId)
-    .then((stream) => new Promise((resolve, reject) => {
-      const writable = fs.createWriteStream(downloadPath);
-      stream.pipe(writable);
-      stream.on('end', () => resolve(downloadPath));
-      stream.on('error', reject);
-    }));
-}
-
-function handleLocation(message, replyToken) {
-  return client.replyMessage(
-    replyToken,
-    {
-      type: 'location',
-      title: message.title,
-      address: message.address,
-      latitude: message.latitude,
-      longitude: message.longitude,
-    }
-  );
-}
-
-function handleSticker(message, replyToken) {
-  return client.replyMessage(
-    replyToken,
-    {
-      type: 'sticker',
-      packageId: message.packageId,
-      stickerId: message.stickerId,
-    }
-  );
-}
-
-// listen on port
-// const port = process.env.PORT || 3000; Heroku 的prot 不一樣
-const port = process.env.PORT || 8080;
-app.listen(port, () => {
-  if (baseURL) {
-    console.log(`listening on ${baseURL}:${port}/callback`);
-  } else {
-    console.log("It seems that BASE_URL is not set. Connecting to ngrok...")
-    ngrok.connect(port, (err, url) => {
-      if (err) throw err;
-
-      baseURL = url;
-      console.log(`listening on ${baseURL}/callback`);
-    });
-  }
+app.post('/ajax', function (request, response) {
+    response.send("response by ajax");
 });
