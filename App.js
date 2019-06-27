34e79b100418343604beaa924f3d3b190704b60f
sohanchang <sohanchang@gmail.com>
2019-06-27 15:48:36 +0800

    reply message version1.0.39

--- App.js
+++ App.js
@@ -10,5 +10,5 @@ const port = process.env.PORT || 8080;
 //binds and listens for connections on the specified host and port. This method is identical to Node’s http.Server.listen().
 //因為 express 預設走 port 3000，而 heroku 上預設卻不是，要透過下列程式轉換
 
-app.listen(port);
-console.log(`MERN Stack listening on ${port}`);
+//app.listen(port);
+//console.log(`MERN Stack listening on ${port}`);
